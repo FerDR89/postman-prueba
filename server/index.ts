5fa8031072aa9d29b9a84f1a1936de1220550ae2
Fernando de Row <ferderow@gmail.com>
2021-10-26 15:20:49 -0300

    API modified

--- server/index.ts
+++ server/index.ts
@@ -3,52 +3,61 @@ import * as express from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.post("/signup/:userEmail", (req, res) => {
-  res.json({
-    userName: "Hector",
-    userId: "11223344",
-    newUser: true,
-  });
-});
-
-app.post("/auth/:userEmail", (req, res) => {
-  res.json({
-    userName: "Hector",
-    userId: "11223344",
-  });
-});
-
-app.get("/users/:userEmail", (req, res) => {
-  res.json({
-    userName: "Hector",
-    userId: "11223344",
-  });
-});
-
 app.get("/products/:userId", (req, res) => {
   res.json({
     userName: "Jose",
-    userProductsList: [],
+    userProductsList: [
+      {
+        aa115544: {
+          productId: "aa115544",
+          productName: "arroz",
+          productPrice: 55,
+          productStock: 100,
+        },
+      },
+      {
+        aa556655: {
+          productId: "aa556655",
+          productName: "fideos",
+          productPrice: 100,
+          productStock: 100,
+        },
+      },
+      {
+        cc880012: {
+          productId: "cc880012",
+          productName: "porotos",
+          productPrice: 200,
+          productStock: 1000,
+        },
+      },
+    ],
   });
 });
 
-app.post("/products/userId/:newProduct", (req, res) => {
+app.post("/products/:userId", (req, res) => {
   res.json({
     userProductsList: [],
+    productId: "11aa5523",
+    productName: "Tomate",
+    productPrice: 200,
+    productStock: 1000,
     message: "Your product was successfuly added",
   });
 });
 
-app.patch("/products/userId/productId", (req, res) => {
+app.patch("/products/:userId/:productId", (req, res) => {
   res.json({
-    userProductsList: [],
+    productId: "11aa5523",
+    productName: "Tomate",
+    productPrice: 200,
+    productStock: 1000,
     message: "Your product was successfuly modified",
   });
 });
 
-app.delete("/products/userId/productId", (req, res) => {
-  res.json({
-    userProductsList: [],
+app.delete("/products/:userId/:productId", (req, res) => {
+  res.status(201).json({
     message: "Your product was successfuly deleted",
   });
 });
